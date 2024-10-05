3b108d420527e24c5918542a3236f548368e0d69
Joe Prakash <joep@nyu.edu>
2024-10-04 23:19:36 -0400

    update stuff

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ print_color() {
       "red")     color_code="0;31" ;;
       "green")   color_code="0;32" ;;
       "yellow")  color_code="0;33" ;;
-      "magenta") color_code="0;35" ;;
       "cyan")    color_code="0;36" ;;
       *)         color_code="0" ;;
   esac
@@ -15,8 +14,8 @@ print_color() {
 
 print_usage() {
   print_color "Usage:" "cyan"
-  print_color "  * For controller: $0 --role controller" "cyan"
-  print_color "  * For worker: $0 --role worker --token <token> --controller-ip <ip>" "cyan"
+  print_color "  - For controller: $0 --role controller"
+  print_color "  - For worker: $0 --role worker --token <token> --controller-ip <ip>"
   exit 1
 }
 
@@ -44,39 +43,43 @@ done
 
 # Validate the role
 if [ "$ROLE" != "controller" ] && [ "$ROLE" != "worker" ] && [ "$ROLE" != "lb" ]; then
-  print_color "Invalid role. Must be either 'controller' or 'worker'." "red"
+  print_color "Invalid role. Available roles are 'controller', 'worker', 'lb'" "red"
   print_usage
+  exit 1
 fi
 
 # If worker, ensure token and controller IP are provided
 if [ "$ROLE" == "worker" ]; then
   if [ -z "$TOKEN" ] || [ -z "$CONTROLLER_IP" ]; then
-      print_color "For worker role, both --token and --controller-ip must be provided." "red"
+      print_color "For worker role, both --token and --controller-ip must be provided" "red"
       print_usage
+      exit 1
   fi
 
 elif [ "$ROLE" == "lb" ]; then
   if [ -z "$NODE_IP" ]; then
-      print_color "For lb role, --node-ip must be provided." "red"
+      print_color "For lb role, --node-ip must be provided" "red"
       print_usage
+      exit 1
   fi
 fi
 
-install_cloud_dep() {
+prepare_cloud_machine() {
   systemctl daemon-reload > /dev/null
+
   # Disable systemd resolved
   systemctl stop systemd-resolved > /dev/null
   systemctl disable systemd-resolved > /dev/null
   rm -f /etc/resolv.conf > /dev/null
   printf "nameserver 1.1.1.1\nnameserver 8.8.8.8\n" > /etc/resolv.conf
 
-  print_color "\xE2\x9C\x94  Disabled systemd resolved" "green"
+  print_color "\xE2\x9C\x94  Disable systemd resolved" "green"
   print_color "\xE2\x9C\x94  Default DNS set to 1.1.1.1 and 8.8.8.8" "cyan"
 
   swapoff -a
   sed -i '/\sswap\s/s/^/#/' /etc/fstab
 
-  print_color "\xE2\x9C\x94  Swap turned off" "green"
+  print_color "\xE2\x9C\x94  Turn off swap" "green"
 
   cat << EOF | tee /etc/modules-load.d/k8s.conf > /dev/null
 overlay
@@ -86,42 +89,40 @@ EOF
   modprobe overlay
   modprobe br_netfilter
 
-  print_color "\xE2\x9C\x94  Added overlay and netfilter modules" "green"
-
-  cat <<EOF | tee /etc/sysctl.d/k8s.conf > /dev/null
-net.bridge.bridge-nf-call-iptables  = 1
-net.bridge.bridge-nf-call-ip6tables = 1
-net.ipv4.ip_forward                 = 1
-EOF
-  print_color "\xE2\x9C\x94  Enabled IPv6" "green"
+  print_color "\xE2\x9C\x94  Add overlay and netfilter kernel modules" "green"
 
   sysctl -w net.ipv4.ip_forward=1 > /dev/null
-  print_color "\xE2\x9C\x94  Enabled network forewarding" "green"
+  print_color "\xE2\x9C\x94  Enable packet forwarding" "green"
 
-  systemctl disable firewalld --now > /dev/null
-  print_color "\xE2\x9C\x94  Disabled Firewall" "green"
+  # Firewall rule
+  interfaces=($(ls /sys/class/net | grep -v lo))
+  firewall-cmd --zone=internal --add-interface="${interfaces[0]}" --permanent > /dev/null
+  firewall-cmd --zone=internal --add-rich-rule='rule family="ipv4" source address="0.0.0.0/0" accept' --permanent > /dev/null
+  firewall-cmd --reload > /dev/null
+
+  print_color "\xE2\x9C\x94  Configure firewall" "green"
 
   dnf install -y NetworkManager NetworkManager-tui > /dev/null
 
   systemctl daemon-reload > /dev/null
 
-  print_color "Installing Kubernetes dependencies" "cyan"
+  print_color "Installing Kubernetes dependencies..." "cyan"
 
   # For CRI
   dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo > /dev/null
   dnf install -y docker-ce docker-ce-cli docker-buildx-plugin docker-compose-plugin > /dev/null
-  dnf install -y containernetworking-plugins container-selinux selinux-policy-base crio iproute-tc > /dev/null
+  dnf install -y containernetworking-plugins container-selinux selinux-policy-base crio iproute-tc git > /dev/null
   dnf install -y kubernetes kubernetes-kubeadm kubernetes-client > /dev/null
   systemctl enable --now kubelet docker crio > /dev/null
-  print_color "\xE2\x9C\x94  Installed Container Runtime dependencies" "green"
+  print_color "\xE2\x9C\x94  Install CRI dependencies" "green"
 
   # For CSI
-  yum --setopt=tsflags=noscripts install iscsi-initiator-utils nfs-utils
+  yum --setopt=tsflags=noscripts install iscsi-initiator-utils > /dev/null
   echo "InitiatorName=$(/sbin/iscsi-iname)" > /etc/iscsi/initiatorname.iscsi
   systemctl enable iscsid > /dev/null
   modprobe iscsi_tcp > /dev/null
   systemctl start iscsid > /dev/null
-  print_color "\xE2\x9C\x94  Installed Container Storage dependencies" "green"
+  print_color "\xE2\x9C\x94  Install CSI dependencies" "green"
 }
 
 generate_haproxy_config() {
@@ -201,13 +202,15 @@ EOF
 
 ## Control Plane
 if [ "$ROLE" == "controller" ]; then
-  install_cloud_dep
+  print_color "Setting up controller node..." "cyan"
 
-  curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+  prepare_cloud_machine
 
-  print_color "Initializing Controller" "cyan"
+  curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash > /dev/null
+
+  print_color "Initializing Controller..." "cyan"
   kubeadm init --cri-socket unix:///var/run/crio/crio.sock --pod-network-cidr=10.244.0.0/16 > /dev/null
-  print_color "\xE2\x9C\x94  Initialized Kubeternetes" "green"
+  print_color "\xE2\x9C\x94  Initialize Kubernetes" "green"
 
   mkdir -p $HOME/.kube > /dev/null
   cp -i /etc/kubernetes/admin.conf $HOME/.kube/config > /dev/null
@@ -217,53 +220,58 @@ if [ "$ROLE" == "controller" ]; then
   print_color "Waiting for cluster to be ready..." "yellow"
   sleep 20
 
-  print_color "Initializing Container Networking Interface" "cyan"
+  print_color "Initializing Container Networking Interface..." "cyan"
   CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/master/stable.txt)
-  curl -L --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-linux-amd64.tar.gz
+  curl -L --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-linux-amd64.tar.gz > /dev/null
   tar xzvfC cilium-linux-amd64.tar.gz /usr/local/bin > /dev/null
   rm -f cilium-linux-amd64.tar.gz > /dev/null
   cilium install > /dev/null
-  print_color "\xE2\x9C\x94  CNI installed" "green"
+  print_color "\xE2\x9C\x94  Install CNI" "green"
+
+  print_color "Installing CNI plugins..." "cyan"
+  kubectl apply -f https://raw.githubusercontent.com/k8snetworkplumbingwg/multus-cni/master/deployments/multus-daemonset-thick.yml > /dev/null
+  print_color "\xE2\x9C\x94  Install CNI plugin" "green"
 
   print_color "Waiting for cluster to be ready..." "yellow"
   sleep 20
 
-  print_color "Initializing Container Runtime Interface" "cyan"
+  print_color "Initializing Container Runtime Interface..." "cyan"
   kubectl apply -f https://raw.githubusercontent.com/kata-containers/kata-containers/stable-3.2/tools/packaging/kata-deploy/kata-rbac/base/kata-rbac.yaml > /dev/null
-  sleep 5
+  sleep 3
   kubectl apply -f https://raw.githubusercontent.com/kata-containers/kata-containers/stable-3.2/tools/packaging/kata-deploy/kata-deploy/base/kata-deploy.yaml > /dev/null
-  kubectl -n kube-system wait --timeout=5m --for=condition=Ready -l name=kata-deploy pod > /dev/null
+  sleep 30
   kubectl apply -f https://raw.githubusercontent.com/kata-containers/kata-containers/main/tools/packaging/kata-deploy/runtimeclasses/kata-runtimeClasses.yaml > /dev/null
-  print_color "\xE2\x9C\x94  CRI installed" "green"
+  print_color "\xE2\x9C\x94  install CRI " "green"
 
-  print_color "Initializing Ingress Controller" "cyan"
+  print_color "Initializing Ingress Controller..." "cyan"
   kubectl apply -f https://raw.githubusercontent.com/osiris-cloud/install/refs/heads/main/ingress-controller.yaml > /dev/null
-  print_color "\xE2\x9C\x94  Ingress Controller installed" "green"
+  print_color "\xE2\x9C\x94  Install Ingress Controller" "green"
 
   WORKER_TOKEN=$(kubeadm token create --print-join-command | awk '{print $5}')
   MY_IP=$(ip -o -4 addr show | grep -v ' lo ' | awk '{print $4}' | head -n 1 | cut -d/ -f1)
 
   print_color "\xE2\x9C\x94  Osiris Controller node is ready" "green"
   print_color "Run the following on worker nodes" "cyan"
-  print_color "curl -sSL https://raw.githubusercontent.com/osiris-cloud/install/refs/heads/main/install.sh | bash -s -- --role worker --token $WORKER_TOKEN --controller-ip $MY_IP" "magenta"
-  echo ""
+  print_color "-> curl -sSL https://get.osiriscloud.io | bash -s -- --role worker --token $WORKER_TOKEN --controller-ip $MY_IP" "yellow"
+  echo "---"
 
-  print_color "Run this after you have bootstrapped your cluster to block subsequent worker node registrations" "yellow"
-  print_color " kubectl delete clusterrolebinding kubeadm:node-autoapprove-bootstrap" "yellow"
+  print_color "To block subsequent worker node registrations, you can run" "cyan"
+  print_color "-> kubectl delete clusterrolebinding kubeadm:node-autoapprove-bootstrap" "yellow"
 
 elif [ "$ROLE" == "worker" ]; then
-  install_cloud_dep
+  print_color "Setting up worker node..." "cyan"
+  prepare_cloud_machine
   kubeadm join $CONTROLLER_IP:6443 --token $TOKEN --cri-socket unix:///var/run/crio/crio.sock --discovery-token-unsafe-skip-ca-verification > /dev/null
 
 elif [ "$ROLE" == "lb" ]; then
-  print_color "Installing docker..." "cyan"
+  print_color "Setting up load balancer..." "cyan"
 
+  print_color "Installing docker..." "cyan"
   dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo > /dev/null
   dnf install -y docker-ce docker-ce-cli docker-buildx-plugin docker-compose-plugin > /dev/null
   systemctl enable docker --now
 
-  print_color "\xE2\x9C\x94  Docker service enabled" "green"
-
+  print_color "\xE2\x9C\x94  Enable Docker service " "green"
   interfaces=($(ls /sys/class/net | grep -v lo))
   cat << EOF | tee /etc/systemd/system/promisc-mode.service > /dev/null
 [Unit]
@@ -278,31 +286,30 @@ ExecStart=/usr/sbin/ip link set ${interfaces[1]} promisc on
 WantedBy=multi-user.target
 EOF
 
-  print_color "\xE2\x9C\x94  Promisc mode enabled on ${interfaces[0]}, ${interfaces[1]}" "green"
+  print_color "\xE2\x9C\x94  Promisc mode enabled on ${interfaces[0]} and ${interfaces[1]}" "green"
   print_color "Configuring firewall rules..." "cyan"
 
   systemctl daemon-reload
   systemctl enable --now promisc-mode.service > /dev/null
 
-  firewall-cmd --zone=internal --add-interface="${interfaces[0]}" --permanent
-  firewall-cmd --zone=internal --add-forward --permanent
+  firewall-cmd --zone=internal --add-interface="${interfaces[0]}" --permanent > /dev/null
+  firewall-cmd --zone=internal --add-forward --permanent > /dev/null
 
-  firewall-cmd --zone=internal --add-rich-rule='rule family="ipv4" source address="0.0.0.0/0" accept' --permanent
-  firewall-cmd --zone=internal --add-rich-rule='rule family="ipv6" source address="::/0" accept' --permanent
+  firewall-cmd --zone=internal --add-rich-rule='rule family="ipv4" source address="0.0.0.0/0" accept' --permanent > /dev/null
 
-  firewall-cmd --reload
+  firewall-cmd --reload > /dev/null
 
-  firewall-cmd --permanent --new-zone=public_int
-  firewall-cmd --zone=public_int --add-interface=ens224 --permanent
-  firewall-cmd --zone=public_int --add-forward --permanent
+  firewall-cmd --permanent --new-zone=public_int > /dev/null
+  firewall-cmd --zone=public_int --add-interface=ens224 --permanent > /dev/null
+  firewall-cmd --zone=public_int --add-forward --permanent > /dev/null
 
-  firewall-cmd --zone=public_int --add-rich-rule='rule family=ipv4 port port=11111 protocol=tcp reject' --permanent
-  firewall-cmd --zone=public_int --add-rich-rule='rule family=ipv4 port port=11111 protocol=udp reject' --permanent
+  firewall-cmd --zone=public_int --add-rich-rule='rule family=ipv4 port port=11111 protocol=tcp reject' --permanent > /dev/null
+  firewall-cmd --zone=public_int --add-rich-rule='rule family=ipv4 port port=11111 protocol=udp reject' --permanent > /dev/null
 
   # block ssh
-  firewall-cmd --zone=public_int --add-rich-rule='rule family=ipv4 port port=22 protocol=tcp reject' --permanent
+  firewall-cmd --zone=public_int --add-rich-rule='rule family=ipv4 port port=22 protocol=tcp reject' --permanent > /dev/null
 
-  firewall-cmd --reload
+  firewall-cmd --reload > /dev/null
 
   print_color "\xE2\x9C\x94  Firewall rules active" "green"
   print_color "- Deny tcp port 11111 on public network" "cyan"
@@ -361,3 +368,5 @@ EOF
   print_color "\xE2\x9C\x94  HAProxy started" "green"
 
 fi
+
+print_color "\xE2\x9C\x94  Setup complete" "green"
